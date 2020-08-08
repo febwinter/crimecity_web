8fe72fae2084f09374c94a52d7b3275cfccae00f
febwinter <skyv2080@gmail.com>
2020-08-08 09:41:44 +0900

    first commit

---
+++ dataGraph/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DatagraphConfig(AppConfig):
+    name = 'dataGraph'
---
+++ mapNnews/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MapnnewsConfig(AppConfig):
+    name = 'mapNnews'

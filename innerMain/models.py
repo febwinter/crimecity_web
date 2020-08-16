64bf28aed9657583858a0a0ef0962aff36be5489
febwinter <skyv2080@gmail.com>
2020-08-16 17:13:15 +0900

    css modified

--- innerMain/models.py
+++ innerMain/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class NewsData(models.Model):
+
+    title = models.CharField(max_length=255, blank=True)
+    image = models.CharField(max_length=255)
+    summary = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "NewsData"
+        verbose_name_plural = "NewsDatas"
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("NewsData_detail", kwargs={"pk": self.pk})
--- innerMain/views.py
+++ innerMain/views.py
@@ -4,7 +4,6 @@ from django.views.generic import View
 
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 
 # Create your views here.
 
@@ -20,7 +19,58 @@ class mainview(LoginRequiredMixin, View):
         }
         return render(request, 'innerMain/welcome_search.html', context)
 
+    def get_link(self, p_url):
+        html = requests.get(p_url).text.strip() # 요청의 결과(응답, response - HTML)를 저장
+        # print(html[0:100]) # 전체 문자열에서 100자만 확인
+
+        # BeautifulSoup 객체를 생성
+        soup = BeautifulSoup(html, 'html5lib')
+
+        # soup.select(css_selector): soup 객체에서 CSS 선택자로 요소들을 찾는 방법
+        r_news_link = soup.select('.coll_cont ul li a.f_link_b')
+        return r_news_link
+
     def post(self,request):
+        
+        
+        url = 'https://search.daum.net/search?w=news&nil_search=btn&DA=NTB&enc=utf8&cluster=y&cluster_page={0}&q={1}'.format(num, keyword)
+        news_link = self.get_link(url)
+        title_list = []
+        link_list = []
+        img_list = []
+        to_check = []
+
+        # title, link
+        for contents in news_link:
+            link = contents.get('href')
+            link_list.append(link)
+            titles = contents.text
+            title_list.append(titles)
+
+        html = requests.get(url).text.strip() 
+        soup = BeautifulSoup(html, 'html5lib')
+        photos = soup.select("div.wrap_thumb div a img[src]")
+
+        for i in photos:
+            img = i["src"]
+            # print(img)
+            # print("===============================")
+            img_list.append(img)
+
+        
+
+        for i in range(10):
+            img =soup.select("#news_img_{0} > div > a > img[src]".format(i))
+            to_check.append(img)
+
+        for i, check in enumerate(to_check):
+            if len(check) == 0:
+                img_list.insert(i, "img_default")
+
+        for i in range(len(title_list)):
+            print("제목 : ",title_list[i])
+            print("링크 : ",link_list[i])
+            print("이미지src : ",img_list[i])
         context = {
             'user' : request.user.username
         }
